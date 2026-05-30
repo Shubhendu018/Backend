14bae0e4c5b8e3cda94ab5316d1e38ca69d38c35
Shubhendu <acharyashubhen@gmail.com>
2026-05-30 16:36:44 +0530

    utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,40 @@
+const asyncHandler =(requestHandler)=>{
+  (req,res,next)=>{
+    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err));
+  }
+}
+
+
+/* const asyncHandler=(fn)=>async(req,res,next)=>{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   try
+    await fn(req,res,next);
+  }catch(error){
+    res.status(errcode||500).json({
+      success:false,
+      message:error.message||"Internal Server Error",
+    })
+  }*/
